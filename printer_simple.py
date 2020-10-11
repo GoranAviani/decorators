c86092a2a1c0c0a6e64b5b035c846c64b97ef955
goranaviani <goranaviani@gmail.com>
2020-10-11 13:17:12 +0200

    printer_simple.py

--- printer_simple.py
+++ printer_simple.py
@@ -1,12 +1,16 @@
 def printer(func):
     def printer_simple(x, y):
         print("Function {} has received  values of {} and {}" .format(func.__name__, x, y))
+        output = func(x, y)
+        print("Output of the {}  function is {}".format(func.__name__, output))
     return printer_simple
 
 @printer
 def some_function(var1, var2):
     #Some work is done here
-    return var1, var2
+    var3 = 5
+    return var3
 
 if __name__ == '__main__':
     result = some_function(1,2)
+    test = result
