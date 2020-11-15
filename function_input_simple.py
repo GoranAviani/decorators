bccf666a86f59de6b0381dc1140a4fefeb5daea4
goranaviani <goranaviani@gmail.com>
2020-11-15 16:45:31 +0100

    funtion_input

--- function_input_simple.py
+++ function_input_simple.py
@@ -1,15 +1,24 @@
 def funtion_input(func):
-    def print_input_type():
-
-            return func()
+    def print_input_type(user_input):
+        try:
+            val = int(user_input)
+            print("Input is an integer number. Number = ", val)
+        except ValueError:
+            try:
+                val = float(user_input)
+                print("Input is a float  number. Number = ", val)
+            except ValueError:
+                print("No.. input is not a number. It's a string")
+            return func(user_input)
     return print_input_type
 
 @funtion_input
-def test_function():
-    print('This is test function!')
+def test_function(user_input):
+    print('This is test function with user input: {}' .format(user_input))
 
 
 def main():
-    test_function()
+    user_input = input('input: ')
+    test_function(user_input)
 if __name__ == '__main__':
     main()
