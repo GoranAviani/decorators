a76c9c0ea0a70733a8b8980e1818c33def8fce7c
goranaviani <goranaviani@gmail.com>
2020-12-16 14:45:15 +0100

    revisit

--- check_if_kwarg_none/kwargs_check_for_none_precise_message.py
+++ check_if_kwarg_none/kwargs_check_for_none_precise_message.py
@@ -11,6 +11,12 @@ def failure_printer(result):
 
 
 def checker_if_kwarg_none(func):
+    """
+    This decorator checks if any value of a given kwarg is None. If any is none decorator stops execution of the code
+    and outputs the needed message.
+    :param func:
+    :return: If no kwarg values are not none: passing kwargs paramethers to a called function
+    """
     def checker(**kwargs):
         for name, value in kwargs.items():
             if value is None:
@@ -25,6 +31,7 @@ def checker_if_kwarg_none(func):
 def some_function(**kwargs):
     var1 = kwargs['var1']
     var2 = kwargs['var2']
+    print(var2)
     return 'ok', var1, var2
 
 def main():
