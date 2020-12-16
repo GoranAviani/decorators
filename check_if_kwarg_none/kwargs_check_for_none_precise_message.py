33373238665d7904fb695164aff59a1896d6e40e
goranaviani <goranaviani@gmail.com>
2020-12-16 14:37:12 +0100

    revisit

--- check_if_kwarg_none/kwargs_check_for_none_precise_message.py
+++ check_if_kwarg_none/kwargs_check_for_none_precise_message.py
@@ -1,19 +1,3 @@
-class color:
-   PURPLE = '\033[95m'
-   CYAN = '\033[96m'
-   DARKCYAN = '\033[36m'
-   BLUE = '\033[94m'
-   GREEN = '\033[92m'
-   YELLOW = '\033[93m'
-   RED = '\033[91m'
-   BOLD = '\033[1m'
-   UNDERLINE = '\033[4m'
-   END = '\033[0m'
-
-SNIPPET_NAME = 'kwargs_check_decorator: '
-ERROR_INTRO_MESSAGE = 'Error message: '
-
-
 def failure_printer(result):
     print(color.RED + SNIPPET_NAME + ERROR_INTRO_MESSAGE +'Variable {} has a value of {} .. (None).'
           .format(result['key_name'], result['result']))
